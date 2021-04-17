3bc75275a86f61b816ab7e3a47909fec41d094a2
Gautam <srinivasrakesh441@gmail.com>
2021-04-17 19:16:47 +0530

    pytest example

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,7 +1,14 @@
+import pytest
 
+
+class CustomError(Exception):
+    def __init__(self,message="value not in range"):
+        self.message = message
+        super().__init__(self.message)
 
 def test_generic():
     a=2
-    b=2
-    assert a==b # if assertion will be true i.e a==b if test case pass
-    assert a!=b
+    with pytest.raises(ValueError):
+        if a not in range(10,20):
+            raise CustomError
+    #assert a==b # if assertion will be true i.e a==b if test case pass
